42e517021a0fa66255042e05b779365629e35bf6
rubel04 <alomrana886@gmail.com>
2025-04-06 05:57:40 +0600

    dynamically show single details food page

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -1,35 +1,29 @@
 const Footer = () => {
   return (
-    <footer className="footer sm:footer-horizontal bg-base-200 text-base-content p-10">
-      <aside>
-        <img className="w-16 object-cover" src="/logo.png" alt="" />
-        <p>
-          TastyFork Ltd.
-          <br />
-          Taste the Magic, One Fork at a Time.
-        </p>
-      </aside>
-      <nav>
-        <h6 className="footer-title">Services</h6>
-        <a className="link link-hover">Branding</a>
-        <a className="link link-hover">Design</a>
-        <a className="link link-hover">Marketing</a>
-        <a className="link link-hover">Advertisement</a>
-      </nav>
-      <nav>
-        <h6 className="footer-title">Company</h6>
-        <a className="link link-hover">About us</a>
-        <a className="link link-hover">Contact</a>
-        <a className="link link-hover">Jobs</a>
-        <a className="link link-hover">Press kit</a>
-      </nav>
-      <nav>
-        <h6 className="footer-title">Legal</h6>
-        <a className="link link-hover">Terms of use</a>
-        <a className="link link-hover">Privacy policy</a>
-        <a className="link link-hover">Cookie policy</a>
-      </nav>
-    </footer>
+    <div className="bg-amber-100 text-base-content">
+      <footer className="footer sm:footer-horizontal md:w-10/12 mx-auto p-10">
+        <aside>
+          <img className="w-16 object-cover" src="/logo.png" alt="" />
+          <p>
+            <span className="text-xl font-medium">TastyFork Ltd.</span>
+            <br />
+            Taste the Magic, One Fork at a Time.
+          </p>
+        </aside>
+        <nav>
+          <h6 className="footer-title">Services</h6>
+          <a className="link link-hover">Products</a>
+          <a className="link link-hover">News</a>
+          <a className="link link-hover">Contact Us</a>
+        </nav>
+        <nav>
+          <h6 className="footer-title">Legal</h6>
+          <a className="link link-hover">Terms of use</a>
+          <a className="link link-hover">Privacy policy</a>
+          <a className="link link-hover">Cookie policy</a>
+        </nav>
+      </footer>
+    </div>
   );
 };
 
