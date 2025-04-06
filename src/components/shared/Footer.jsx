8081d433e1fe079c8b7b3b09c63308a1100781cb
rubel04 <alomrana886@gmail.com>
2025-04-06 06:23:45 +0600

    feat: click the purchase button, and open the  purchase page

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -1,26 +1,56 @@
+import { FaFacebook, FaInstagramSquare, FaTwitter } from "react-icons/fa";
 const Footer = () => {
   return (
     <div className="bg-amber-100 text-base-content">
       <footer className="footer sm:footer-horizontal md:w-10/12 mx-auto p-10">
         <aside>
           <img className="w-16 object-cover" src="/logo.png" alt="" />
-          <p>
-            <span className="text-xl font-medium">TastyFork Ltd.</span>
+          <p className="text-gray-600">
+            <span className="text-2xl font-medium">TastyFork Ltd.</span>
             <br />
             Taste the Magic, One Fork at a Time.
           </p>
         </aside>
         <nav>
-          <h6 className="footer-title">Services</h6>
-          <a className="link link-hover">Products</a>
-          <a className="link link-hover">News</a>
-          <a className="link link-hover">Contact Us</a>
+          <h6 className="text-xl text-black font-medium">Services</h6>
+          <a className="link link-hover text-base text-gray-600">Products</a>
+          <a className="link link-hover text-base text-gray-600">News</a>
+          <a className="link link-hover text-base text-gray-600">Contact Us</a>
         </nav>
         <nav>
-          <h6 className="footer-title">Legal</h6>
-          <a className="link link-hover">Terms of use</a>
-          <a className="link link-hover">Privacy policy</a>
-          <a className="link link-hover">Cookie policy</a>
+          <h6 className="text-xl text-black font-medium">Legal</h6>
+          <a className="link link-hover text-base text-gray-600">
+            Terms of use
+          </a>
+          <a className="link link-hover text-base text-gray-600">
+            Privacy policy
+          </a>
+          <a className="link link-hover text-base text-gray-600">
+            Cookie policy
+          </a>
+        </nav>
+        <nav>
+          <h6 className="text-xl text-black font-medium">Social Links</h6>
+          <div className="flex gap-3 text-2xl">
+            <a
+              href="https://www.facebook.com/tastyfork"
+              className="text-2xl text-blue-500 hover:text-blue-600 transition cursor-pointer"
+            >
+              <FaFacebook />
+            </a>
+            <a
+              href="https://www.instagram.com/tastyfork"
+              className="text-2xl text-[#d62976] hover:text-[#962fbf] transition cursor-pointer"
+            >
+              <FaInstagramSquare />
+            </a>
+            <a
+              href="https://x.com/tastyfork"
+              className="text-2xl text-blue-500 hover:text-blue-600 transition cursor-pointer"
+            >
+              <FaTwitter />
+            </a>
+          </div>
         </nav>
       </footer>
     </div>
